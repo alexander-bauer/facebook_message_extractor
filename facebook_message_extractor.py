85f457b5dffbade7ce60ccf9d49bd3d431d5c57f
Alexander Bauer <sasha@linux.com>
2017-06-30 00:13:58 -0400

    Extract message content, too

--- facebook_message_extractor.py
+++ facebook_message_extractor.py
@@ -31,6 +31,30 @@ def main(args):
         orig_users = [u.strip() for u in html_thread.div.previous_sibling.split(',')]
         thread['users'] = [user_aliases.get(u, u) for u in orig_users]
 
+        thread['messages'] = []
+        for html_message in html_thread.find_all('div', 'message'):
+            message = {}
+
+            header = html_message.find('div', 'message_header')
+            raw_author = header.find('span', 'user').text.strip()
+            message['author'] = user_aliases.get(raw_author, raw_author)
+            message['date'] = header.find('span', 'meta').text.replace('\n', ' ').strip()
+
+            # For some ABSURD reason, content is outside the message div. We
+            # iterate over p tags in the next siblings until we encounter
+            # another message. This is probably only 1, but better safe than sorry.
+            message['content'] = ''
+            for sibling in html_message.next_siblings:
+                if type(sibling) is not bs4.element.Tag:
+                    continue
+                elif sibling.name == 'p':
+                    message['content'] += sibling.text.replace('\n', ' ').strip() + '\n'
+                else:
+                    break
+            message['content'] = message['content'].strip()
+
+            thread['messages'].append(message)
+
         # Construct ID from unique collection of users.
         thread_id = ','.join(sorted(orig_users))
         threads[aliases['threads'].get(thread_id, thread_id)] = thread
@@ -55,6 +79,9 @@ def main(args):
         with open(aliasfile, 'w') as f:
             json.dump(aliases, f, sort_keys=True, indent="    ")
 
+    with open(args.savefile, 'w') as f:
+        json.dump(threads, f, sort_keys=True, indent="    ")
+
 def apply_alias(id, aliases):
     if id in aliases['users']:
         return aliases[id]
@@ -65,6 +92,7 @@ def parse(args=None):
     parser = argparse.ArgumentParser()
     parser.add_argument("messages_html", type=argparse.FileType('r'))
     parser.add_argument("--aliasfile", "-a", type=argparse.FileType('r'), default=None)
+    parser.add_argument("--savefile", "-o", default="messages.json")
     parser.add_argument("--prompt_aliases", "-p", action='store_true')
     return parser.parse_args(args)
 
